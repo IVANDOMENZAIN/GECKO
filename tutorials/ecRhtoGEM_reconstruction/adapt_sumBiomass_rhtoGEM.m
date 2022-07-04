39b4cb30b51bc6ad6b25d027d3c446f7e031730f
Ivan Domenzain <ivand@chalmers.se>
2022-07-04 14:03:16 +0200

    chore: update live script for sumBiomass adaptation

--- tutorials/ecRhtoGEM_reconstruction/adapt_sumBiomass_rhtoGEM.m
+++ tutorials/ecRhtoGEM_reconstruction/adapt_sumBiomass_rhtoGEM.m
@@ -18,8 +18,8 @@ open ../../geckomat/limit_proteins/sumBioMass.m
 %% 2.- Explore biomass and macromolecular synthesis pseudoreactions in rhtoGEM
 % The biomass pseudoreaction, has been identified and explored previously by 
 % the |explore_rhtoGEM.xlm| live script in this tutorial. In order to analyse 
-% its structure, the name of the biomass reaction and the ones for production 
-% of its main precursors can be retrieved from the previously created |getParameters.m| 
+% its structure, the name of the biomass reaction, and the ones for production 
+% of its main precursors, can be retrieved from the previously created |getParameters.m| 
 % script by executing it
 
 cd rhto_scripts
@@ -32,9 +32,10 @@ load('../models/rhtoGEM.mat')
 constructEquations(model,parameters.bioRxn)
 %% 
 % Display the main macromolecular synthesis pseudoreactions and get a list 
-% of their constituent metabolites
+% of their constituent metabolites for the protein, carbohydrate RNA and DNA groups, 
+% as indicated in the |sumBioMass.m| script (lines 19-55).
 
-for macroM = [{'protein'} {'carbohydrate'} {'lipid'} {'RNA'} {'DNA'}]
+for macroM = [{'protein'} {'carbohydrate'} {'RNA'} {'DNA'}]
     disp([macroM{1} ' precursors:'])
     macroM_pos = find(strcmp(model.metNames,macroM));
     reaction   = find(model.S(macroM_pos,:)>0);
@@ -45,3 +46,13 @@ for macroM = [{'protein'} {'carbohydrate'} {'lipid'} {'RNA'} {'DNA'}]
     precTable  = table(IDs,metName)
     disp(' ')
 end
+%% 3.- Update the sumBiomass.m function for the rhtoGEM case
+% Now that all the relevant molecular components for biomass production have 
+% been identified, the list of metabolites in lines 19-55 of the |sumBioMass| 
+% script can be updated. It can be observed that all biomass precursors in the 
+% |rhtoGEM |are also present in |yeastGEM |and use the same metabolite IDs, therefore, 
+% only those metabolites present in the sumBiomass metabolites list that are not 
+% part of rhtoGEM need to be removed.
+% 
+% Save the updated script under the same name in the |GECKO/tutorials/ecRhtoGEM_reconstruction/rhto_scripts| 
+% directory.

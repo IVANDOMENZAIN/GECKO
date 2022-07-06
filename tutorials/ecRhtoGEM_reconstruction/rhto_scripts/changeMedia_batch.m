926a40207f61dd769c4931cceccc3333c112f3ce
Ivan Domenzain <ivand@chalmers.se>
2022-07-06 18:37:03 +0200

    chore: adapt changeMedia_batch.m to ecRhtoGEM

--- tutorials/ecRhtoGEM_reconstruction/rhto_scripts/changeMedia_batch.m
+++ tutorials/ecRhtoGEM_reconstruction/rhto_scripts/changeMedia_batch.m
@@ -1,43 +1,33 @@
-function model = changeMedia_batch(model,c_source)
+function model = changeMedia_batch(model,c_source,flux)
 %changeMedia_batch
+%   function that modifies the ecModel and makes it suitable for batch growth
+%   simulations on different carbon sources.
 %
-% Function that modifies the ecModel and makes it suitable for batch growth
-% simulations on the carbon source of choice. You can add more changes in
-% this function if you need to do so for your model.
+%   model       (struct) An enzyme constrained model
+%   c_source    (string) Rxn name for the main carbon source uptake reaction
+%   flux        (doule) Experimental flux value [mmol/gDw h] for the main 
+%               carbon source uptake reaction.
 %
-% INPUT:
-%   model       An enzyme constrained model.
-%	c_source	The name of the exchange reaction that supplies the model
-%				with carbon.
 %
-% OUTPUT:
-%   model       The enzyme constrained model with modified boundaries.
+%   Usage: model = changeMedia_batch(model,c_source,flux)
 %
-% Usage: model = changeMedia_batch(model,c_source)
-%
-% Benjamin J. Sanchez	2018-12-11
-% Ivan Domenzain        2020-02-07
+% Ivan Domenzain        2022-07-06
 
+% Give the carbon source (c_source) input variable with the following
+% format: c_source  = 'D-glucose exchange (reversible)'
+if nargin<3
+    flux = 1000;
+end
 %first block any uptake
 [rxnIDs,exchange]  = getExchangeRxns(model);
 exchange           = exchange(find(contains(rxnIDs,'_REV')));
 model.ub(exchange) = 0;
 %Allow main carbon source uptake
 c_id  = model.rxns(strcmp(model.rxnNames,c_source));
-model = setParam(model,'ub',c_id,Inf);
-%block glucose and oxygen production
-model.ub(strcmp(model.rxnNames,'oxygen exchange'))    = 0;
-model.ub(strcmp(model.rxnNames,'D-glucose exchange')) = 0;
-%Allow uptake of essential components
-model = setParam(model, 'ub', 'r_1654_REV', Inf); % 'ammonium exchange';
-model = setParam(model, 'ub', 'r_2100_REV', Inf); % 'water exchange' ;
-model = setParam(model, 'ub', 'r_1992_REV', Inf); % 'oxygen exchange';
-model = setParam(model, 'ub', 'r_2049_REV', Inf); % sodium exchange
-model = setParam(model, 'ub', 'r_1861_REV', Inf); % 'iron(2+) exchange';
-model = setParam(model, 'ub', 'r_2005_REV', Inf); % 'phosphate exchange';
-model = setParam(model, 'ub', 'r_2060_REV', Inf); % 'sulphate exchange';
-model = setParam(model, 'ub', 'r_1832_REV', Inf); % 'H+ exchange' ;
-model = setParam(model, 'ub', 'r_2020_REV', Inf); % potassium exchange
-%Allow biomass production 
-model = setParam(model, 'ub', 'r_2111', Inf); % growth
+model = setParam(model,'ub',c_id,flux);
+model = setParam(model, 'ub', 'r_1861_REV', +1000); % 'iron(2+) exchange';
+model = setParam(model, 'ub', 'r_2060_REV', +1000); % 'sulphate exchange';
+model = setParam(model, 'ub', 'r_1714', 0);         % block D-glucose production';
+model = setParam(model, 'ub', 'r_1992', 0);         % block oxygen production';
+model = setParam(model, 'ub', 'r_2111', +1000);     % allow biomass production
 end
